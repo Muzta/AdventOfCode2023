31549fcab41f7cc71aa0681675eb5b230f1bd3d1
muzta <javyngs@gmail.com>
2023-12-05 17:48:58 +0100

    Finished day 4 part 2

--- day04/day04.js
+++ day04/day04.js
@@ -27,7 +27,36 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  console.log(`Solution part 2 is: ${null}`);
+  const cardPoints = {};
+
+  lines.forEach((line) => {
+    const [card, numbers] = line.split(":");
+    const [winning, mine] = numbers
+      .split("|")
+      .map((n) => n.split(" ").filter(Number));
+    const cardId = Number(card.split(" ").find(Number));
+
+    // Initialize the points of the current card
+    const initialPoints = cardPoints[cardId] || 0;
+    cardPoints[cardId] = initialPoints + 1;
+
+    const nWinningNumbers = mine.filter((number) =>
+      winning.includes(number)
+    ).length;
+
+    // For each winning number found in the player's numbers...
+    for (let i = 1; i <= nWinningNumbers; i++) {
+      cardPoints[cardId + i] =
+        // ... calculate the total points for the current card copy: sum the quantity of current card copies and the existing points of the next card
+        cardPoints[cardId] + (cardPoints[cardId + i] || 0);
+    }
+  });
+
+  const sumPoints = Object.values(cardPoints).reduce(
+    (sum, points) => sum + points,
+    0
+  );
+  console.log(`Solution part 2 is: ${sumPoints}`);
 };
 
 part1();
