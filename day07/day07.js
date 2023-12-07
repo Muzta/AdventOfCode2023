b02806bec75bcc30bad617803bcd1742ae8ddf9d
muzta <javyngs@gmail.com>
2023-12-07 21:00:20 +0100

    Finished day 7 part 1

---
+++ day07/day07.js
@@ -0,0 +1,86 @@
+const { readFileSync } = require("fs");
+const lodash = require("lodash");
+
+const lines = readFileSync("day07.txt", { encoding: "utf-8" }) // read day??.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim() // Remove starting/ending whitespace
+  .split("\n"); // Split on newline
+
+const part1 = () => {
+  // Decide how strong the hand is according to its characters
+  const getHandStrength = (hand) => {
+    const occurrencesByChar = lodash.countBy(hand.split(""));
+    const differentCharsValues = Object.values(occurrencesByChar);
+    switch (differentCharsValues.length) {
+      case 1:
+        return 7; // Five of a kind
+      case 2:
+        return differentCharsValues.includes(4) ? 6 : 5; // Four of a kind or Full house
+      case 3:
+        return differentCharsValues.includes(3) ? 4 : 3; // Three of a kind or Two pair
+      case 4:
+        return 2; // One pair
+      default:
+        return 1; // High card
+    }
+  };
+
+  // Custom comparator to apply to lines array
+  const compareHands = (first, second) => {
+    const firstHand = first.split(" ")[0].trim();
+    const secondHand = second.split(" ")[0].trim();
+    const firstStrength = getHandStrength(firstHand);
+    const secondStrength = getHandStrength(secondHand);
+    if (firstStrength > secondStrength) return 1;
+    if (firstStrength < secondStrength) return -1;
+
+    // Same value hands, strongest first card check
+    const cardStrengths = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "T",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+
+    // This is a simple "for" so it can exit the compareHands functino as a whole, not possible with "forEach"
+    for (let index = 0; index < first.length; index++) {
+      const firstCard = first[index];
+      const secondCard = second[index];
+      const firstHandCardStrength = cardStrengths.indexOf(firstCard);
+      const secondHandCardStrength = cardStrengths.indexOf(secondCard);
+
+      if (firstHandCardStrength > secondHandCardStrength) return 1;
+      if (firstHandCardStrength < secondHandCardStrength) return -1;
+    }
+
+    // If all cards are equal, return 0
+    return 0;
+  };
+
+  const sortedHands = lines.sort(compareHands); // Sort the hands, from weaker to stronger
+
+  const totalWinnings = sortedHands
+    .map((hand, index) => {
+      const handBid = hand.split(" ")[1].trim(); // Take the bid...
+      return handBid * (index + 1); // ... and multiply by its rank
+    })
+    .reduce((sum, bidRank) => sum + bidRank, 0); // And sum to get the total winnings
+
+  console.log(`Solution part 1 is: ${totalWinnings}`);
+};
+
+const part2 = () => {
+  console.log(`Solution part 2 is: ${null}`);
+};
+
+part1();
+part2();
